9d3bbc0a2c1a3967f053e6a452bb6b15b480bc42
Justin Madilia <jmadilia96@gmail.com>
2023-11-13 21:52:59 -0500

    Add test account login info to login page.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,15 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Login',
 };
- 
+
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <main className="flex flex-col items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
@@ -16,6 +17,11 @@ export default function LoginPage() {
           </div>
         </div>
         <LoginForm />
+      </div>
+      <div className={`${lusitana.className} flex flex-col items-center justify-center rounded-lg bg-gray-50 px-6 pb-4 pt-8 text-xl`}>
+        Use this test account! <span role="img" aria-label="smile">😀</span>
+        <span>Email: user@nextemail.com</span>
+        <span>Password: 123456</span>
       </div>
     </main>
   );
